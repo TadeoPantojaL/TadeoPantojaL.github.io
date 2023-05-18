aa179006f8add1ed6ad7e12e6b713280f2df93e5
Tadeo_Pantoja <bohemianrob47@gmail.com>
2023-05-18 02:51:15 -0600

    s

--- login.js
+++ login.js
@@ -1,11 +1,13 @@
-function login(){
-    var username = document.getElementById("username").value;
+function login() {
+    var username = document.getElementById("user").value;
     var password = document.getElementById("password").value;
-    if(username == "admin" && password == "admin"){
-        window.location = "index.html";
-        return false;
+  
+    // Realiza la validación de los datos ingresados (puedes personalizar esta parte según tus necesidades)
+    if (username === "usuario" && password === "contraseña") {
+      // Si la validación es exitosa, redirige al usuario a la página de usuario
+      window.location.href = "d2.html";
+    } else {
+      // Si la validación falla, muestra un mensaje de error o realiza alguna acción adicional
+      alert("Usuario o contraseña incorrectos");
     }
-    else{
-        alert("Login failed");
-    }
-}
+  }
