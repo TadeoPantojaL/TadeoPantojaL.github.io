dfcdf21c797cf52922590461b683f1d5ae4a9730
Tadeo_Pantoja <bohemianrob47@gmail.com>
2023-05-24 10:38:54 -0600

    RENEW

--- js/logout.js
+++ js/logout.js
@@ -1,6 +1,6 @@
 const username = JSON.parse(localStorage.getItem('login_sucess')) || false
 if (!username) {
-    window.location.href = '/login.html'
+    window.location.href = '/index.html'
 }
 
 const logout = document.querySelector('#logout_button')  
@@ -8,6 +8,6 @@ const logout = document.querySelector('#logout_button')
 logout.addEventListener('click', () => {
     alert('Sesión cerrada')
     localStorage.removeItem('login_sucess')
-    window.location.href = '/login.html'
+    window.location.href = '/index.html'
   
 })
