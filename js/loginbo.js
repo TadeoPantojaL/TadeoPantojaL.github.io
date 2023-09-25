b5a5d99e42b2a64500bdd3f63b5f2a670f716e2c
Tadeo_Pantoja <bohemianrob47@gmail.com>
2023-09-25 06:21:49 -0600

    backoffice added
    bo

---
+++ js/loginbo.js
@@ -0,0 +1,36 @@
+const user = document.getElementById('bousername');
+const pass = document.getElementById('bopassword');
+const button = document.getElementById('boton');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  const data = {
+    username: user.value.toLowerCase(),
+    password: pass.value
+  }
+  const bousuarios = {
+    
+    'admin': { nombre: 'Tadeo Pantoja', password: 'admin' },
+    'ggonzalez1': { nombre: 'Georgina Flores', password: '12345' },
+    'ibarron1': { nombre: 'Jonathan Barrón', password: '12345' },
+    'lmartinez': { nombre: 'Liliana Martinez', password: '12345' },
+    'ceperez': { nombre: 'Cecilia Perez', password: '12345' },
+    'asegundo': { nombre: 'Angelica Segundo', password: '12345' },
+    'anunez': { nombre: 'Anayeli Segundo', password: '12345' },
+    'cmariscal': { nombre: 'Carlos Mariscal', password: '12345' },
+    'asanchez': { nombre: 'Arturo Sanchez', password: '12345' },
+    'kgomez': { nombre: 'Kevin Gomez', password: '12345' },
+    'cperalta': { nombre: 'Carlos Peralta', password: '12345' },
+    'egonzalez': { nombre: 'Jesus Gonzalez', password: '12345' },
+    'cocampo': { nombre: 'Cynthia Zamora', password: '12345' },
+    'icadena': { nombre: 'Imanol Cadena', password: '12345' },
+    'jpliego': { nombre: 'Jonatyhan Pliego', password: '12345' }
+  }
+  if (data.username in bousuarios && data.password === bousuarios[data.username].password) {
+    window.location.href = "/html/boarchive.html";
+    localStorage.setItem("bologin_sucess", JSON.stringify(usuarios[data.username]));
+  } else {
+    alert("Credenciales incorrectas");
+  }
+
+})
--- js/logout copy.js
+++ js/logout copy.js
@@ -1,4 +1,4 @@
-const username = JSON.parse(localStorage.getItem('login_sucess')) || false
+const username = JSON.parse(localStorage.getItem('bologin_sucess')) || false
 if (!username) {
     window.location.href = '/index.html'
     
@@ -8,6 +8,6 @@ const logout = document.querySelector('#logout_button')
 
 logout.addEventListener('click', () => {
     alert('Sesión cerrada')
-    localStorage.removeItem('login_sucess')
+    localStorage.removeItem('bologin_sucess')
     window.location.href = '/index.html'
 })
