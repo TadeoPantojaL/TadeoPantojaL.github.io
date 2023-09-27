50cbe8f9a9c7dc583ba86bf3fe7ddd9a90271050
Tadeo_Pantoja <bohemianrob47@gmail.com>
2023-09-27 00:42:11 -0600

    t

--- php/getCodigos.php
+++ php/getCodigos.php
@@ -4,8 +4,8 @@ $con = new Database();
 $pdo = $con->conectar();
 
 $campo = $_POST['campo'];
-
-$sql = "SELECT DNS FROM dn WHERE DNS LIKE ? ORDER BY DNS ASC LIMIT 5";
+//SELECT * FROM bzngdotbkt2uyzmnaqk2.dn;
+$sql = "SELECT bzngdotbkt2uyzmnaqk2 FROM dn WHERE bzngdotbkt2uyzmnaqk2 LIKE ? ORDER BY bzngdotbkt2uyzmnaqk2 ASC LIMIT 5";
 //SELECT * FROM `dn` WHERE `DNS` LIKE '%221130%' ORDER BY `DNS` ASC
 
 $query = $pdo->prepare($sql);
@@ -30,7 +30,7 @@ if ($query->rowCount() > 0) {
     $html = "<ul>";
 
     while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        $html .= "<li onclick=\"mostrar('" . $row['DNS'] . "')\">" . $row['DNS'] . "</li>";
+        $html .= "<li onclick=\"mostrar('" . $row['bzngdotbkt2uyzmnaqk2'] . "')\">" . $row['bzngdotbkt2uyzmnaqk2'] . "</li>";
     }
 
     $html .= "</ul>";
